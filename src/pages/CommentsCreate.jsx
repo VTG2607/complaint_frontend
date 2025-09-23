a6883060eadbf134757d65e23952511becaf559c
VTG2607 <tr260705@gmail.com>
2025-09-23 15:16:53 +0200

    changed wrong url

--- src/pages/CommentsCreate.jsx
+++ src/pages/CommentsCreate.jsx
@@ -12,7 +12,7 @@ export function CommentsCreate({ complaintId }) {
         try {
             const token = localStorage.getItem("authToken");
             const response = await api.post(
-                `https://complaint-backend-4863a97516ff.herokuapp.com//api/complaint/${complaintId}/comments/`,
+                `https://complaint-backend-4863a97516ff.herokuapp.com/api/complaint/${complaintId}/comments/`,
                 data,
                 {
                     headers: {
